4135af4a3c0ad94a6f889d182c15565202a242f1
JoaoPauloCMarra <joaopaulocmarra@gmail.com>
2020-04-27 09:28:21 +0200

    0.62 warning fix

--- Collapsible.js
+++ Collapsible.js
@@ -93,7 +93,13 @@ export default class Collapsible extends Component {
               () => callback(this.props.collapsedHeight)
             );
           } else {
-            this._getNode().measure((x, y, width, height) => {
+            let ref;
+            if (typeof this.contentHandle.measure === 'function') {
+              ref = this.contentHandle;
+            } else {
+              ref = this.contentHandle.getNode();
+            }
+            ref.measure((x, y, width, height) => {
               this.setState(
                 {
                   measuring: false,
